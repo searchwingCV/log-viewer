1ee232595875e6e7f5cd2423ad48e96401392567
Ricardo Martinez <ricardomg@riseup.net>
2022-09-05 11:09:10 +0200

    Add CRUD operations for /mission endpoint

--- server/app/schemas/base.py
+++ server/app/schemas/base.py
@@ -76,6 +76,7 @@ class Page(AbstractPage[T], Generic[T]):
 
 class GeoPoint(BaseSchema):
     class Config:
+        orm_mode = True
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
 
