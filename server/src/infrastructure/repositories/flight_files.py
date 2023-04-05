0ec8583806c55f425490132007530f72e89786f9
Ricardo Martinez <ricardomg@riseup.net>
2023-04-05 11:48:10 -0600

    feat: add methods to get file in flight_file repository

--- server/src/infrastructure/repositories/flight_files.py
+++ server/src/infrastructure/repositories/flight_files.py
@@ -1,7 +1,8 @@
 from typing import List
 
 from domain.flight_file.entities import FlightFile, ID_Type
-from infrastructure.db.orm import FlightFiles as FlightFilesModel
+from domain.flight_file.value_objects import AllowedFiles
+from infrastructure.db.orm import FlightFile as FlightFilesModel
 from infrastructure.repositories.base import BaseRepository
 from sqlalchemy.orm import Session
 
@@ -11,5 +12,14 @@ class FlightFileRepository(BaseRepository):
     _entity = FlightFile
 
     def get_by_flight(self, fk_flight: ID_Type, session: Session) -> List[FlightFile]:
-        data = self._get_by_filters(session, {"fk_flight": fk_flight})
-        return data
+        result = session.query(self._model).filter_by(**{"fk_flight": fk_flight}).all()
+        return [self._model_to_schema(row) for row in result]
+
+    def get_by_flight_and_type(
+        self, fk_flight: ID_Type, file_type: AllowedFiles, session: Session
+    ) -> FlightFile | None:
+        result = session.query(self._model).filter_by(**{"fk_flight": fk_flight, "file_type": file_type}).first()
+        if result:
+            return self._model_to_schema(result)
+        else:
+            return None
