cfb64acbbf59b4a5bbb91ac539d8e14809bfe67b
Ricardo Martinez <ricardomg@riseup.net>
2023-04-05 11:48:10 -0600

    feat: add methods to get file in flight_file repository

--- server/tests/integration/conftest.py
+++ server/tests/integration/conftest.py
@@ -1,6 +1,9 @@
+import random
+
 import pytest
 from common.config import TestConfig
-from infrastructure.db.orm import Base, Drone
+from domain.flight_file.value_objects import AllowedFiles
+from infrastructure.db.orm import Base, Drone, Flight, FlightFile, Mission
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -23,14 +26,76 @@ def test_db_session():
 
 @pytest.fixture
 def insert_drones(test_db_session, get_sample_drone):
-    def _insert_drones(number=10):
+    def wrapper(number=10):
         for _ in range(number):
             drone = Drone(**get_sample_drone().dict())
             test_db_session.add(drone)
             test_db_session.commit()
             test_db_session.refresh(drone)
 
-    return _insert_drones
+    return wrapper
+
+
+@pytest.fixture
+def insert_flights(test_db_session, get_sample_flight):
+    def wrapper(nb_drones, nb_flights, nb_missions):
+        for _ in range(nb_flights):
+            flight = Flight(
+                **get_sample_flight(
+                    drone_id=random.randint(1, nb_drones),
+                    mission_id=random.choice([None, random.randint(1, nb_missions)]),
+                ).dict()
+            )
+            test_db_session.add(flight)
+            test_db_session.commit()
+            test_db_session.refresh(flight)
+
+    return wrapper
+
+
+@pytest.fixture
+def insert_missions(test_db_session, get_sample_mission):
+    def wrapper(nb_missions):
+        for _ in range(nb_missions):
+            mission = Mission(**get_sample_mission().dict())
+            test_db_session.add(mission)
+            test_db_session.commit()
+            test_db_session.refresh(mission)
+
+    return wrapper
+
+
+@pytest.fixture
+def insert_flight_files(test_db_session, get_sample_flight_file):
+    def wrapper(nb_flights, chances_missing_file, allowed_files):
+        for flight_id in range(1, nb_flights + 1):
+            for file_type in allowed_files:
+                # simulate that some flights do not contain one or more files
+                if random.random() < chances_missing_file:
+                    continue
+                flight_file = FlightFile(**get_sample_flight_file(flight_id, file_type).dict())
+                test_db_session.add(flight_file)
+                test_db_session.commit()
+                test_db_session.refresh(flight_file)
+
+    return wrapper
+
+
+@pytest.fixture
+def fill_mock_data(insert_flights, insert_drones, insert_missions, insert_flight_files):
+    def _fill_mock_data(
+        nb_drones=10,
+        nb_flights=40,
+        nb_missions=5,
+        chances_missing_file=0.15,
+        allowed_files=[AllowedFiles.apm, AllowedFiles.log, AllowedFiles.rosbag, AllowedFiles.tlog],
+    ):
+        insert_drones(nb_drones)
+        insert_missions(nb_missions)
+        insert_flights(nb_drones, nb_flights, nb_missions)
+        insert_flight_files(nb_flights, chances_missing_file, allowed_files)
+
+    return _fill_mock_data
 
 
 @pytest.fixture(scope="function")
---
+++ server/tests/integration/infrastructure/repositories/test_flight_file_repository.py
@@ -0,0 +1,61 @@
+import pytest
+from domain.flight_file.entities import BaseFlightFile, FlightFile
+from domain.flight_file.value_objects import AllowedFiles
+from infrastructure.repositories import FlightFileRepository
+
+
+@pytest.fixture
+def add_data(test_db_session):
+    def wrapper(file_type, flight_id, filename):
+        path = f"{flight_id}/{file_type}/{filename}"
+        sample_file = BaseFlightFile(file_type=file_type, fk_flight=flight_id, location=path)
+        sample_file_db = FlightFile(**sample_file.dict())
+        test_db_session.add(sample_file_db)
+        test_db_session.commit()
+
+    return wrapper
+
+
+def test_get_by_flight(fill_mock_data, test_db_session):
+    allowed_files = [
+        AllowedFiles.apm,
+        AllowedFiles.log,
+        AllowedFiles.rosbag,
+        AllowedFiles.tlog,
+    ]
+    fill_mock_data(nb_flights=10, chances_missing_file=0, allowed_files=allowed_files)
+    repository = FlightFileRepository()
+    flight_files = repository.get_by_flight(1, test_db_session)
+    assert len(flight_files) == len(allowed_files)
+    assert all([isinstance(flight_file, FlightFile) for flight_file in flight_files])
+
+
+def test_get_by_flight_does_not_exist(fill_mock_data, test_db_session):
+    allowed_files = [
+        AllowedFiles.apm,
+        AllowedFiles.log,
+        AllowedFiles.rosbag,
+        AllowedFiles.tlog,
+    ]
+    fill_mock_data(nb_flights=10, chances_missing_file=1, allowed_files=allowed_files)
+    repository = FlightFileRepository()
+    flight_files = repository.get_by_flight(1, test_db_session)
+    assert flight_files == []
+
+
+@pytest.mark.parametrize(
+    ["flight_id", "should_return_none", "file_type"],
+    [
+        (1, True, AllowedFiles.tlog),
+        (1, False, AllowedFiles.log),
+        (40, True, AllowedFiles.log),
+    ],
+)
+def test_get_by_flight_and_type(fill_mock_data, test_db_session, flight_id, should_return_none, file_type):
+    allowed_files = [AllowedFiles.apm, AllowedFiles.log, AllowedFiles.rosbag]
+
+    fill_mock_data(nb_flights=10, chances_missing_file=0, allowed_files=allowed_files)
+
+    repository = FlightFileRepository()
+    flight_file = repository.get_by_flight_and_type(flight_id, file_type, test_db_session)
+    assert (flight_file is None) == should_return_none
