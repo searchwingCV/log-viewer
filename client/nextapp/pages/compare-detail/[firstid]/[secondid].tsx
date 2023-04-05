37cc3437e824affc3fb29c258f65387e49af4408
Mai Ly Le <mailyle@MBP-von-Mai.fritz.box>
2023-04-05 12:55:37 +0200

    removes async declaration in dynamic paths for now

--- client/nextapp/pages/compare-detail/[firstid]/[secondid].tsx
+++ client/nextapp/pages/compare-detail/[firstid]/[secondid].tsx
@@ -17,7 +17,7 @@ const FlightCompareScreen = ({}) => {
   )
 }
 
-export async function getStaticPaths() {
+export function getStaticPaths() {
   const data = getFlightsMock()
 
   const paths = data.map((item) => ({
