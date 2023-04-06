791b381eab140610dea9f9be48e1f8d8a538662e
Ricardo Martinez <ricardomg@riseup.net>
2023-04-06 12:27:55 -0600

    fix: add file endpoint

--- server/src/presentation/rest/controllers/file.py
+++ server/src/presentation/rest/controllers/file.py
@@ -6,24 +6,23 @@ from fastapi.responses import StreamingResponse
 from presentation.rest.dependencies import get_file_service
 
 router = APIRouter(
-    prefix="",
-    tags=["root"],
+    prefix="/file",
+    tags=["file"],
     responses={404: {"description": "Not found"}},
-    include_in_schema=False,
 )
 
 logger = get_logger(__name__)
 
 
-@router.get("/file/{id}", response_class=StreamingResponse)
+@router.get("/{id}", response_class=StreamingResponse)
 def download_file(id: str, file_service: FileService = Depends(get_file_service)):
     io_file = file_service.get_file(id)
     try:
-        filename = io_file.flight_file.split("/")[-1]
+        filename = io_file.flight_file.location.split("/")[-1]
         return StreamingResponse(
             content=io_file.io,
             media_type="application/octet-stream",
             headers={"Content-Disposition": f"attachment; filename={filename}"},
         )
-    except FileNotFoundError | NotFoundException:
+    except (FileNotFoundError, NotFoundException):
         raise HTTPException(status.HTTP_404_NOT_FOUND, "File not found")
