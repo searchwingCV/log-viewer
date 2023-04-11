ff314284fc357766200dc69f48e16e53512be82f
Ricardo Martinez <ricardomg@riseup.net>
2023-04-11 11:39:29 -0600

    chore: improve error messages in patch request

--- server/src/presentation/rest/mixins.py
+++ server/src/presentation/rest/mixins.py
@@ -1,9 +1,10 @@
 import typing as t
 
 from application.services.base import BaseCRUDService
+from common.exceptions.db import DuplicatedKeyError, ForeignKeyNotFound
 from common.logging import get_logger
 from presentation.rest.serializers import APISerializer, UpdateSerializer
-from presentation.rest.serializers.errors import EntityNotFoundError
+from presentation.rest.serializers.errors import EntityNotFoundError, UniqueViolationError, UnprocessableEntityError
 from presentation.rest.serializers.responses import BatchUpdateResponse
 
 logger = get_logger(__name__)
@@ -18,7 +19,21 @@ class CRUDMixin:
     ) -> BatchUpdateResponse[T]:
         response = BatchUpdateResponse(items=[])
         for entry in items_to_update.items:
-            updated_item = service.update(entry)
+
+            try:
+                updated_item = service.update(entry)
+            except DuplicatedKeyError:
+                response.errors.append(
+                    UniqueViolationError(id=entry.id, detail=f"The resource name already exists -> {entry}")
+                )
+                continue
+            except ForeignKeyNotFound:
+                foreign_keys = entry.get_foreign_keys()
+                response.errors.append(
+                    UnprocessableEntityError(id=entry.id, detail=f"one or more references do not exist: {foreign_keys}")
+                )
+                continue
+
             if updated_item is None:
                 response.errors.append(
                     EntityNotFoundError(
