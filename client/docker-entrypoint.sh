e00f9f57ce60178e04739dbfc249c849a8f36285
Mai Ly Le <mailyle@MBP-von-Mai.fritz.box>
2023-04-12 13:08:39 +0200

    deletes duplicate file

--- client/docker-entrypoint.sh
+++ client/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # no verbose
+#coming from https://raphaelpralat.medium.com/system-environment-variables-in-next-js-with-docker-1f0754e04cde
+#this file is important for exposing production env variables by 
+# exchanging occurences of the env variables in the .next folder with system 
+
 set +x
 # config
 envFilename='.env.production'
@@ -21,7 +25,7 @@ function apply_path {
     # if config found
     if [ -n "$configValue" ] && [ -n "$envValue" ]; then
       # replace all
-      echo "Replace: ${configValue} with: ${envValue}"
+      echo "Replace: ${configValue} in production env with: ${envValue} from local env file"
       find $nextFolder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#$configValue#$envValue#g"
     fi
   done < $envFilename
