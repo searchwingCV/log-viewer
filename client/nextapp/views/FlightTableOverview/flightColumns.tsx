7122380da82c50075d775ec67a47de8ee5c73d51
Mai Ly Le <mailyle@MBP-von-Mai.fritz.box>
2023-05-23 16:07:18 +0200

    fixes wrong import

--- client/nextapp/views/FlightTableOverview/flightColumns.tsx
+++ client/nextapp/views/FlightTableOverview/flightColumns.tsx
@@ -7,7 +7,7 @@ import {
   determineWidth,
   TippyValueWrapper,
 } from '~/modules/TableComponents'
-import type { TableFlightSerializer } from './FlightTableOverView'
+import type { TableFlightSerializer } from './FlightTableOverview'
 
 export const flightColumns = (
   missionOptions?: { name: string; value: number }[],
