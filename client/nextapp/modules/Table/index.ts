9b41b64d9a52787477159fd3f3f808a31dea3027
Mai Ly Le <mailyle@MacBook-Pro-von-Mai.local>
2023-09-07 13:54:59 +0200

    deletes separate table pages and moves logic into only one table component, refactors imports

---
+++ client/nextapp/modules/Table/index.ts
@@ -0,0 +1,18 @@
+import { GlobalTextFilter } from './components/GlobalTextFilter'
+import { Pagination } from './components/Pagination'
+import { SelectCheckbox } from './components/SelectCheckbox'
+import { ColumnFilter } from './components/ColumnFilter'
+import { TableHead } from './components/TableHead'
+import { TableBody } from './components/TableBody'
+import { TextInputCell } from './components/TextInputCell'
+import { DateInputCell } from './components/DateInputCell'
+import { SelectInputCell } from './components/SelectInputCell'
+import { CustomizeColumnsDrawer } from './components/CustomizeColumnsDrawer'
+import { determineWidth } from './functions'
+import { ToggleCustomizeOrder } from './components/ToggleCustomizeOrder'
+import TippyValueWrapper from './components/TippyValueWrapper'
+import { missionColumns } from "./columns/missionColumns";
+import { flightColumns } from "./columns/flightColumns";
+import { droneColumns } from './columns/droneColumns'
+import { Table } from './Table'
+export { TippyValueWrapper, ToggleCustomizeOrder, GlobalTextFilter, Pagination, SelectCheckbox, ColumnFilter, TableBody, TableHead, TextInputCell, SelectInputCell, DateInputCell, CustomizeColumnsDrawer, determineWidth, droneColumns, flightColumns, missionColumns, Table }
