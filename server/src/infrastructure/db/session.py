f55d3a2c89fa422ac741c4eccf9e2f5f84d2f960
Ricardo Martinez <ricardomg@riseup.net>
2023-10-23 17:28:27 +0200

    chore(config): increase overflow for sqlalchemy connection pool

--- server/src/infrastructure/db/session.py
+++ server/src/infrastructure/db/session.py
@@ -8,7 +8,12 @@ from sqlalchemy.orm import sessionmaker
 
 class SessionContextManager:
     def __init__(self, config: Optional[Type[BaseConfig]] = get_current_config()) -> None:
-        self._engine = create_engine(config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+        self._engine = create_engine(
+            config.SQLALCHEMY_DATABASE_URI,
+            pool_pre_ping=True,
+            pool_size=config.SQLALCHEMY_POOL_SIZE,
+            max_overflow=config.SQLALCHEMY_MAX_OVERFLOW,
+        )
         self._session_maker = sessionmaker(autocommit=False, autoflush=False, bind=self._engine)
 
     def __enter__(self, *args, **kwargs) -> sqlalchemySession:
