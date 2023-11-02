1929625c11f265f3b378b608e2849bbb663d5131
Mai Ly Le <mailyle@MBP-von-Mai.fritz.box>
2023-11-02 07:55:33 +0100

    feat(files): fix endpoint urls for deletion and uploading

--- client/nextapp/api/flight/deleteFile.ts
+++ client/nextapp/api/flight/deleteFile.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const deleteFile = async ({ url }: { url: string }) => {
-    const response = await axios.delete(url)
+    const response = await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}${url}`)
         .then((res) => {
             return res.data
         })
